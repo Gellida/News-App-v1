58ad2ad24d3a1712f151926620680e4a31cb20d7
Gellida <gellida7@gmail.com>
2022-07-07 15:13:16 -0500

    added a Presentation Screen

---
+++ app/src/main/java/jg/apps/newsapi/MainFragment.kt
@@ -0,0 +1,30 @@
+package jg.apps.newsapi
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : Fragment() {
+    private lateinit var viewModel: MainViewModel
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        // TODO: Use the ViewModel
+    }
+
+}
